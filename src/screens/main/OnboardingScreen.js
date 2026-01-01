05c5755bd6fae258493c45c2ed9f4b4e964f8d2a
Jesus Prada <jesusprada177@gmail.com>
2026-01-01 14:08:21 -0400

    feat: increase padding for skip button in OnboardingScreen for better visibility

--- src/screens/main/OnboardingScreen.js
+++ src/screens/main/OnboardingScreen.js
@@ -161,7 +161,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "flex-end",
     paddingHorizontal: 20,
-    paddingTop: 10,
+    paddingTop: 50,
   },
   skipButton: {
     paddingVertical: 8,
